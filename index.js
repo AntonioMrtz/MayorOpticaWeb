df8f7e832003b92a318c549edcfcd9b7aff179ae
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-02-26 22:53:29 +0100

    sticky button

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
-  
-  window.addEventListener("scroll", reveal);
+}
+
+window.addEventListener("scroll", reveal);
+
+
+
+const el = document.getElementById("home");
+const sticky = document.getElementById("sticky-button-container");
+const sticky_1 = document.getElementById("sticky-button");
+const observer = new IntersectionObserver((entries) => {
+
+    console.log(entries[0].isIntersecting)
+
+    if(entries[0].isIntersecting){
+         
+      sticky.classList.remove("scroll")
+      sticky_1.classList.remove("scroll")
+    } else {
+         // el is not visible
+         sticky.classList.add("scroll")
+         sticky_1.classList.add("scroll")
+
+    }
+});
+
+observer.observe(el); // Asynchronous call
+
+
+
+$(window).on('resize', function () {
+  var conwidth = $('.container').width() / 2 - 30;
+  $('.sticky-button').css('left', 'calc(50% + ' + conwidth + 'px)');
+});
+
+
+
 
