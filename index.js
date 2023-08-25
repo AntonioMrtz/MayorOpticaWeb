50a8c8ea47809dec2ba7ac89169a52be296a40cd
AntonioMrtz <antoniomartinezfernandez17@gmail.com>
2023-08-25 17:42:02 +0200

    feat : add animation on hover sticky button

--- index.js
+++ index.js
@@ -33,30 +33,3 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 observer.observe(el); // Asynchronous call
-
-/* CALCULATE HEIGHT HOME WHOLE SCREEN*/
-
-/* const PHONE_MEDIA_QUERY = window.matchMedia("(max-width: 990px)");
-
-const containerHome = document.getElementById("container-home");
-const containerNavbar = document.getElementById("home");
-
-function setHeightContainerHom() {
-    if (!PHONE_MEDIA_QUERY.matches) {
-        let windowHeight = window.innerHeight;
-        let navbarHeight = window
-            .getComputedStyle(containerNavbar)
-            .getPropertyValue("height")
-            .replace("px", "");
-        containerHome.style.height = windowHeight - navbarHeight + "px";
-
-    }
-}
-
-window.addEventListener("resize", () => {
-    setHeightContainerHom();
-});
-
-window.addEventListener("load", () => {
-    setHeightContainerHom();
-}); */
