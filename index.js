d9749498175b35f491b0d4673ece7db989588745
Antonio Martinez <antoniomartinezfernandez17@gmail.com>
2024-08-11 00:03:35 +0200

    style: run style format

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /* REVEAL ANIMATION */
 
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+  var reveals = document.querySelectorAll(".reveal");
 
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
 
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
+  }
 }
 
 window.addEventListener("scroll", reveal);
@@ -22,14 +22,14 @@ const el = document.getElementById("home");
 const sticky = document.getElementById("sticky-button-container");
 const sticky_1 = document.getElementById("sticky-button");
 const observer = new IntersectionObserver((entries) => {
-    if (entries[0].isIntersecting) {
-        sticky.classList.remove("scroll");
-        sticky_1.classList.remove("scroll");
-    } else {
-        // el is not visible
-        sticky.classList.add("scroll");
-        sticky_1.classList.add("scroll");
-    }
+  if (entries[0].isIntersecting) {
+    sticky.classList.remove("scroll");
+    sticky_1.classList.remove("scroll");
+  } else {
+    // el is not visible
+    sticky.classList.add("scroll");
+    sticky_1.classList.add("scroll");
+  }
 });
 
 observer.observe(el); // Asynchronous call
